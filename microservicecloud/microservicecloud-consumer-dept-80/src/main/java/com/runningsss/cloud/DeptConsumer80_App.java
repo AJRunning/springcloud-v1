44ed082a00aaf43dab2885b5b95a1756d0a4e117
liqings <1182512371@qq.com>
2019-02-23 12:34:20 +0800

    ribbon的初步配置

--- microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/runningsss/cloud/DeptConsumer80_App.java
+++ microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/runningsss/cloud/DeptConsumer80_App.java
@@ -2,12 +2,14 @@ package com.runningsss.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author liqings
  * @date 2019-02-19
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class DeptConsumer80_App {
 
     public static void main(String args[]) {
--- microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/runningsss/cloud/cfgbean/ConfigBean.java
+++ microservicecloud/microservicecloud-consumer-dept-80/src/main/java/com/runningsss/cloud/cfgbean/ConfigBean.java
@@ -1,7 +1,7 @@
 package com.runningsss.cloud.cfgbean;
 
 
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -14,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 public class ConfigBean {
 
     @Bean
+    @LoadBalanced
    public RestTemplate getRestTemplate(){
        return new RestTemplate();
    }
